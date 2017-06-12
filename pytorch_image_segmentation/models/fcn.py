19612d542170c11c5e65cec36bde38c75d25338e
Daniil Pakhomov <warmspringwinds@gmail.com>
2017-06-11 21:47:37 -0400

    updated gitignore and fcn 32s

--- pytorch_image_segmentation/models/fcn.py
+++ pytorch_image_segmentation/models/fcn.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 import torchvision.models as models
+import torch.utils.model_zoo as model_zoo
+from torchvision.models.vgg import model_urls
 
 
 class FCN_32s(nn.Module):
@@ -10,18 +12,35 @@ class FCN_32s(nn.Module):
         super(FCN_32s, self).__init__()
         
         vgg16 = models.vgg16(pretrained=True,
-                             fully_conv=True,
-                             num_classes=num_classes)
+                             fully_conv=True)
         
+        # Copy all the features
         self.features = vgg16.features
-        self.classifier = vgg16.classifier
+        
+        # Remove the last classification 1x1 convolution
+        fully_conv = list(vgg16.classifier.children())
+        fully_conv = fully_conv[:-1]
+        self.fully_conv = nn.Sequential(*fully_conv)
+        
+        # Get a new 1x1 convolution and randomly initialize
+        score_32s = nn.Conv2d(4096, num_classes, 1)
+        self._normal_initialization(score_32s)
+        self.score_32s = score_32s
+        
+        
+    def _normal_initialization(self, layer):
+        
+        layer.weight.data.normal_(0, 0.01)
+        layer.bias.data.zero_()
         
     def forward(self, x):
         
         input_spatial_dim = x.size()[2:]
         
         x = self.features(x)
-        x = self.classifier(x)
+        x = self.fully_conv(x)
+        x = self.score_32s(x)
+        
         x = nn.functional.upsample_bilinear(input=x, size=input_spatial_dim)
         
         return x
