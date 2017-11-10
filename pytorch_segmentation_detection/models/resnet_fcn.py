2ca756957a877f0597680a6f866276cafe512f2a
Daniil Pakhomov <warmspringwinds@gmail.com>
2017-11-10 10:55:36 -0500

    added unet and benchmarked it

--- pytorch_segmentation_detection/models/resnet_fcn.py
+++ pytorch_segmentation_detection/models/resnet_fcn.py
@@ -11,9 +11,6 @@ class Resnet18_8s(nn.Module):
         
         super(Resnet18_8s, self).__init__()
         
-        ## Here we load the standart resnet, remove logit layer,
-        ## and add some layers as a part of ACT block
-        
         # Load the pretrained weights, remove avg pool
         # layer and get the output stride of 8
         resnet18_32s = torchvision.models.resnet18(fully_conv=True,
@@ -83,8 +80,6 @@ class Resnet34_8s(nn.Module):
         
         super(Resnet34_8s, self).__init__()
         
-        ## Here we load the standart resnet, remove logit layer,
-        ## and add some layers as a part of ACT block
         
         # Load the pretrained weights, remove avg pool
         # layer and get the output stride of 8
@@ -131,6 +126,76 @@ class Resnet34_8s(nn.Module):
         logits_16s = self.score_16s(x)
         
         x = self.resnet34_32s.layer4(x)
+        logits_32s = self.score_32s(x)
+        
+        logits_16s_spatial_dim = logits_16s.size()[2:]
+        logits_8s_spatial_dim = logits_8s.size()[2:]
+                
+        logits_16s += nn.functional.upsample_bilinear(logits_32s,
+                                        size=logits_16s_spatial_dim)
+        
+        logits_8s += nn.functional.upsample_bilinear(logits_16s,
+                                        size=logits_8s_spatial_dim)
+        
+        logits_upsampled = nn.functional.upsample_bilinear(logits_8s,
+                                                           size=input_spatial_dim)
+        
+        return logits_upsampled
+    
+
+class Resnet50_8s(nn.Module):
+    
+    
+    def __init__(self, num_classes=2):
+        
+        super(Resnet50_8s, self).__init__()
+        
+        
+        # Load the pretrained weights, remove avg pool
+        # layer and get the output stride of 8
+        resnet50_32s = torchvision.models.resnet50(fully_conv=True,
+                                                   pretrained=True,
+                                                   output_stride=32,
+                                                   remove_avg_pool_layer=True)
+        
+        resnet_block_expansion_rate = resnet50_32s.layer1[0].expansion
+        
+        # Create a linear layer -- we don't need logits in this case
+        resnet50_32s.fc = nn.Sequential()
+        
+        self.resnet50_32s = resnet50_32s
+        
+        self.score_32s = nn.Conv2d(512 *  resnet_block_expansion_rate,
+                                   num_classes,
+                                   kernel_size=1)
+        
+        self.score_16s = nn.Conv2d(256 *  resnet_block_expansion_rate,
+                                   num_classes,
+                                   kernel_size=1)
+        
+        self.score_8s = nn.Conv2d(128 *  resnet_block_expansion_rate,
+                                   num_classes,
+                                   kernel_size=1)
+        
+        
+    def forward(self, x):
+        
+        input_spatial_dim = x.size()[2:]
+        
+        x = self.resnet50_32s.conv1(x)
+        x = self.resnet50_32s.bn1(x)
+        x = self.resnet50_32s.relu(x)
+        x = self.resnet50_32s.maxpool(x)
+
+        x = self.resnet50_32s.layer1(x)
+        
+        x = self.resnet50_32s.layer2(x)
+        logits_8s = self.score_8s(x)
+        
+        x = self.resnet50_32s.layer3(x)
+        logits_16s = self.score_16s(x)
+        
+        x = self.resnet50_32s.layer4(x)
         logits_32s = self.score_32s(x)
         
         logits_16s_spatial_dim = logits_16s.size()[2:]
---
+++ pytorch_segmentation_detection/models/unet.py
@@ -0,0 +1,186 @@
+import torch
+import torch.nn as nn
+
+
+
+def conv3x3(in_planes, out_planes):
+    "3x3 convolution with padding"
+    
+    return nn.Conv2d(in_planes,
+                     out_planes,
+                     kernel_size=3,
+                     padding=1,
+                     bias=True)
+
+
+class UnetDownBlock(nn.Module):
+    """ Downsampling block of Unet.
+    
+        The whole architercture of Unet has a one common pattern: a block
+        that spatially downsamples the input followed by two layers of 3x3 convolutions that
+        has 'inplanes' number of input planes and 'planes' number of channels.
+    
+    """
+    
+    def __init__(self, inplanes, planes, predownsample_block):
+        
+        super(UnetDownBlock, self).__init__()
+        
+        self.predownsample_block = predownsample_block
+        self.conv1 = conv3x3(inplanes, planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        
+        
+    def forward(self, x):
+        
+        x = self.predownsample_block(x)
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        
+        return x
+
+
+class UnetUpBlock(nn.Module):
+    """ Downsampling block of Unet.
+    
+        The whole architercture of Unet has a one common pattern: a block
+        that has two layers of 3x3 convolutions that
+        has 'inplanes' number of input planes and 'planes' number of channels,
+        followed by 'postupsample_block' which increases the spatial resolution
+    
+    """
+    
+    def __init__(self, inplanes, planes, postupsample_block=None):
+        
+        super(UnetUpBlock, self).__init__()
+        
+        self.conv1 = conv3x3(inplanes, planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        
+        if postupsample_block is None:
+        
+            self.postupsample_block = torch.nn.ConvTranspose2d(in_channels=planes,
+                                                               out_channels=planes/2,
+                                                               kernel_size=2,
+                                                               stride=2)
+        else:
+            
+            self.postupsample_block = postupsample_block
+        
+        
+    def forward(self, x):
+        
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.postupsample_block(x)
+        
+        return x
+        
+
+class Unet(nn.Module):
+    """Unet network. ~297 ms on hd image."""
+    
+    def __init__(self, num_classes=2):
+        
+        super(Unet, self).__init__()
+        
+        self.predownsample_block = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.identity_block = nn.Sequential()
+        
+        self.block1 = UnetDownBlock(
+                                    predownsample_block=self.identity_block,
+                                    inplanes=3,
+                                    planes=64,
+                                    )
+            
+        self.block2_down = UnetDownBlock(
+                                         predownsample_block=self.predownsample_block,
+                                         inplanes=64,
+                                         planes=128,
+                                         )
+        
+        self.block3_down = UnetDownBlock(
+                                         predownsample_block=self.predownsample_block,
+                                         inplanes=128,
+                                         planes=256
+                                        )
+        
+        self.block4_down = UnetDownBlock(
+                                         predownsample_block=self.predownsample_block,
+                                         inplanes=256,
+                                         planes=512
+                                        )
+        
+        self.block5_down = UnetDownBlock(
+                                         predownsample_block=self.predownsample_block,
+                                         inplanes=512,
+                                         planes=1024
+                                         )
+        
+        self.block1_up = torch.nn.ConvTranspose2d(in_channels=1024,
+                                                  out_channels=512,
+                                                  kernel_size=2,
+                                                  stride=2)
+        
+        self.block2_up = UnetUpBlock(
+                                    inplanes=1024,
+                                    planes=512
+                                    )
+        
+        self.block3_up = UnetUpBlock(
+                                    inplanes=512,
+                                    planes=256
+                                    )
+        
+        self.block4_up = UnetUpBlock(
+                                    inplanes=256,
+                                    planes=128
+                                    )
+        
+        self.block5 = UnetUpBlock(
+                                  inplanes=128,
+                                  planes=64,
+                                  postupsample_block=self.identity_block
+                                 )
+        
+        self.logit_conv = nn.Conv2d(64,
+                                    num_classes,
+                                    kernel_size=1)
+        
+    
+    def forward(self, x):
+        
+        input_spatial_dim = x.size()[2:]
+        
+        # Left part of the U figure in the Unet paper
+        features_1s_down = self.block1(x)
+        features_2s_down = self.block2_down(features_1s_down)
+        features_4s_down = self.block3_down(features_2s_down)
+        features_8s_down = self.block4_down(features_4s_down)
+        
+        # Bottom part of the U figure in the Unet paper
+        features_16s = self.block5_down(features_8s_down)
+        
+        # Right part of the U figure in the Unet paper
+        features_8s_up = self.block1_up(features_16s)
+        features_8s_up = torch.cat([features_8s_down, features_8s_up], dim=1)
+        
+        features_4s_up = self.block2_up(features_8s_up)
+        features_4s_up = torch.cat([features_4s_down, features_4s_up], dim=1)
+        
+        features_2s_up = self.block3_up(features_4s_up)
+        features_2s_up = torch.cat([features_2s_down, features_2s_up], dim=1)
+        
+        features_1s_up = self.block4_up(features_2s_up)
+        features_1s_up = torch.cat([features_1s_down, features_1s_up], dim=1)
+        
+        features_final = self.block5(features_1s_up)
+        
+        logits = self.logit_conv(features_final)
+            
+        return logits
