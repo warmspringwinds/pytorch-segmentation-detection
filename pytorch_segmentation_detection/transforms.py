58abf317e999b9a434f1c3316c7c03c4c197c5ba
Daniil Pakhomov <warmspringwinds@gmail.com>
2017-11-15 12:22:44 -0500

    added a utility function to be able to create one-hot encoding

--- pytorch_segmentation_detection/transforms.py
+++ pytorch_segmentation_detection/transforms.py
@@ -119,6 +119,25 @@ def pad_to_fit_tiles_pil(image, tile_size):
     
     return adjusted_img, adjusted_size_in_pixels, adjusted_size_in_tiles
 
+
+def convert_labels_to_one_hot_encoding(labels, number_of_classes):
+
+    labels_dims_number = labels.dim()
+
+    # Add a singleton dim -- we need this for scatter
+    labels_ = labels.unsqueeze(labels_dims_number)
+    
+    # We add one more dim to the end of tensor with the size of 'number_of_classes'
+    one_hot_shape = list(labels.size())
+    one_hot_shape.append(number_of_classes)
+    one_hot_encoding = torch.zeros(one_hot_shape)
+    
+    # Filling out the tensor with ones
+    one_hot_encoding.scatter_(dim=labels_dims_number, index=labels_, value=1)
+    
+    return one_hot_encoding
+
+
 class ComposeJoint(object):
     
     def __init__(self, transforms):
