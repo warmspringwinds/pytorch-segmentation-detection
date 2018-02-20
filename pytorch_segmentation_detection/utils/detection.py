5f05065290d3eba4f1614c9664d540a0263cada4
Daniil Pakhomov <warmspringwinds@gmail.com>
2018-02-19 21:05:08 -0500

    added documentation to the bbox format conversion functions

--- pytorch_segmentation_detection/utils/detection.py
+++ pytorch_segmentation_detection/utils/detection.py
@@ -2,6 +2,21 @@ import torch
 
 
 def convert_bbox_xywh_tensor_to_xyxy(bbox_xywh_tensor):
+    """Function to convert bounding boxes in format (x_topleft, y_topleft, width, height)
+    to a format of (x_min, y_min, x_max, y_max).
+    
+    Works with a tensors of a (N, 4) shape.
+    
+    Parameters
+    ----------
+    bbox_xywh_tensor : FloatTensor of shape (N, 4)
+        Tensor with bounding boxes in xywh format
+        
+    Returns
+    -------
+    bbox_xyxy_tensor : FloatTensor of shape (N, 4)
+        Tensor with bounding boxes in xyxy format
+    """
     
     bbox_xyxy_tensor = bbox_xywh_tensor.clone()
     
