e2e44dd9f1168114e668ee65b8d417703cc8c4c6
Daniil Pakhomov <warmspringwinds@gmail.com>
2018-03-15 14:34:58 -0400

    changed the official cityscapes labeling slightly

--- pytorch_segmentation_detection/utils/cityscapes.py
+++ pytorch_segmentation_detection/utils/cityscapes.py
@@ -102,7 +102,7 @@ labels = [
     Label(  'train'                , 31 ,       16 , 'vehicle'         , 7       , True         , False        , (  0, 80,100) ),
     Label(  'motorcycle'           , 32 ,       17 , 'vehicle'         , 7       , True         , False        , (  0,  0,230) ),
     Label(  'bicycle'              , 33 ,       18 , 'vehicle'         , 7       , True         , False        , (119, 11, 32) ),
-    Label(  'license plate'        , -1 ,       -1 , 'vehicle'         , 7       , False        , True         , (  0,  0,142) ),
+    Label(  'license plate'        , -1 ,       255 , 'vehicle'         , 7       , False        , True         , (  0,  0,142) ),
 ]
 
 
