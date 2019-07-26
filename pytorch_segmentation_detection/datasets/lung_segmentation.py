5b03b40dec9b8ff46472fc9d14556372646950f1
Daniil Pakhomov <warmspringwinds@gmail.com>
2019-07-25 22:24:40 -0400

    added lung dataset dataloader and rle conversion utils

---
+++ pytorch_segmentation_detection/datasets/lung_segmentation.py
@@ -0,0 +1,68 @@
+import glob
+import sys, os
+import pydicom
+import numpy as np
+import pandas as pd
+import torch.utils.data as data
+
+from ..utils.rle_mask_encoding import rle2mask
+
+
+class LungSegmentation(data.Dataset):
+    
+    
+    def __init__(self,
+                 train_images_folder_path=None,
+                 annotation_csv_file_path=None,
+                 train=True,
+                 joint_transform=None):
+        
+        self.joint_transform = joint_transform
+                
+        self.train_images_folder_path = train_images_folder_path
+        
+        
+        images_filenames = sorted(glob.glob(train_images_folder_path + "/*/*/*.dcm"))
+        
+        train_portion = 0.8
+        num_train = len(images_filenames)
+        split = int(np.floor(train_portion * num_train))
+        
+        if train:
+            
+            self.images_filenames = images_filenames[:split]
+            
+        else:
+            
+            self.images_filenames = images_filenames[split:]
+        
+        
+        self.annotation_df = pd.read_csv(annotation_csv_file_path)
+        
+            
+    def __len__(self):
+        
+        return len(self.images_filenames)
+    
+    def __getitem__(self, index):
+        
+        image_filename = self.images_filenames[index]
+        
+        image = pydicom.dcmread(image_filename).pixel_array
+        
+        image_filename_stripped = image_filename.split('/')[-1][:-4]
+        
+        annotation_rle = self.annotation_df.loc[self.annotation_df['ImageId'] == image_filename_stripped][' EncodedPixels'].values[0]
+        
+        if annotation_rle != ' -1':
+        
+            annotation = rle2mask(annotation_rle, width=1024, height=1024 )
+        else:
+            
+            annotation = np.zeros((1024, 1024))
+        
+        if self.joint_transform is not None:
+            
+            image, annotation = self.joint_transform([image, annotation])
+            
+        return image, annotation
---
+++ pytorch_segmentation_detection/utils/rle_mask_encoding.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+# Borrowed from:
+# https://www.kaggle.com/c/siim-acr-pneumothorax-segmentation/data
+
+def mask2rle(img, width, height):
+    rle = []
+    lastColor = 0;
+    currentPixel = 0;
+    runStart = -1;
+    runLength = 0;
+
+    for x in range(width):
+        for y in range(height):
+            currentColor = img[x][y]
+            if currentColor != lastColor:
+                if currentColor == 255:
+                    runStart = currentPixel;
+                    runLength = 1;
+                else:
+                    rle.append(str(runStart));
+                    rle.append(str(runLength));
+                    runStart = -1;
+                    runLength = 0;
+                    currentPixel = 0;
+            elif runStart > -1:
+                runLength += 1
+            lastColor = currentColor;
+            currentPixel+=1;
+
+    return " ".join(rle)
+
+
+def rle2mask(rle, width, height):
+    mask= np.zeros(width* height)
+    array = np.asarray([int(x) for x in rle.split()])
+    starts = array[0::2]
+    lengths = array[1::2]
+
+    current_position = 0
+    for index, start in enumerate(starts):
+        current_position += start
+        mask[current_position:current_position+lengths[index]] = 255
+        current_position += lengths[index]
+
+    return mask.reshape(width, height)
